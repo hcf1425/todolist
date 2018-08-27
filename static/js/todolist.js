b7f38dd7ebad79c43c5f2575b942ffef402f5323
hcf1425 <hcf1425@163.com>
2018-08-28 02:54:13 +0800

    从html文件中抽取css和js脚本并建立文件来存储。

---
+++ static/js/todolist.js
@@ -0,0 +1,52 @@
+/**
+ * Created by huangchengfang on 2018/8/28.
+ */
+
+$(function(){
+		// 增加按钮单击，增加数据到html；删除，上下单击
+		$('#btn1').click(function(){
+			var vals = $('#txt1').val()
+			if(vals == '')
+			{
+				alert('请输入内容')
+				return
+			}
+			var $li = $('<li><span>'+ vals +'</span><a href="javascript:;" class="up"> ↑ </a><a href="javascript:;" class="down"> ↓ </a><a href="javascript:void(0);" class="del">删除</a></li>')
+			$('#list').prepend( $li )
+		})
+
+		// $('a').click(function(){
+		// 	alert(1)
+		// }) **** 原始绑定命令的方法无法给未来元素绑定命令
+		$('#list').delegate('a', 'click', function(){
+			// alert(2)
+			// 如果删除，执行删除标签li -- class值
+			var myclass = $(this).prop('class')
+			// alert(myclass)
+			if(myclass == 'del')
+			{
+				// 删除自己的父级
+				$(this).parent().remove()
+			}
+			if(myclass == 'up')
+			{
+				// 如果是第一个提示
+				if($(this).parent().index() == 0){
+					alert('已经是第一个了')
+					return
+				}
+				$(this).parent().insertBefore( $(this).parent().prev() )
+			}
+			if(myclass == 'down')
+			{
+				// 如果是最后一个提示:li下标 == 长度-1
+				// 后面没有别的li了就是最后一个，后面的所有人的长度==0
+				if($(this).parent().nextAll().length == 0)
+				{
+					alert('已经是最后一个了')
+					return
+				}
+				$(this).parent().insertAfter( $(this).parent().next() )
+			}
+		})
+	})
