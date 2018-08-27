c66eb8c2f82c84c9ed6413a24f88186c27add7e6
hcf1425 <hcf1425@163.com>
2018-08-28 02:36:40 +0800

    flask_script集成，导入Manager，创建manager脚本管理器对象。

--- manage.py
+++ manage.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template
+from flask_script import Manager
 
 app = Flask(__name__)
+
+
+# 创建脚本管理器对象
+manager= Manager(app)
+
 
 
 @app.route('/',methods=["get",'post'])
@@ -8,4 +14,4 @@ def index():
     return render_template('todolist.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
