fff1debb2e8dc6a32f19b0f25389508e00e29679
hcf1425 <hcf1425@163.com>
2018-10-08 21:51:00 +0800

    中文编码问题解决！

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
+# coding: utf-8
 from flask_migrate import Migrate,MigrateCommand
-from flask_script import Manager
+import flask_script
 from apps import create_app,db
 from apps.utils import models  #这句话不能注释，否则迁移的时候会找不到模型类。报 no change detected
 
@@ -7,7 +8,7 @@ from apps.utils import models  #这句话不能注释，否则迁移的时候会
 app = create_app()
 
 # 创建脚本管理器对象
-manager= Manager(app)
+manager= flask_script.Manager(app)
 
 # 迁移时让app和db建立关联
 Migrate(app,db)
